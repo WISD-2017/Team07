f8d999fa8dfb3f96c56880c4073ed9026ede6e32
CHIA-YU TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-12-30 13:33:58 +0800

    新增連接至search.blade的路由

--- routes/web.php
+++ routes/web.php
@@ -15,26 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/search', function () {
+    return view('search');
+});
+
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/aa/a1', function () {
-    return view('aa.a1');
-});
-Route::get('/search/searchpost', function () {
-    return view('search.searchpost');
-});
-Route::get('/search/searchend', function () {
-    return view('search.searchend');
-});
 
-Route::get('/booking/bookingsearch', function () {
-    return view('bookingsearch');
-});
-Route::get('/booking/bookingcancel', function () {
-    return view('bookingcancel');
-});
-
-Route::get('/booking/test', function () {
-    return view('test');
-});
