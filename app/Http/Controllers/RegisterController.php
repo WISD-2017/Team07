f6ee35fa343f17313d923268a2d55120812ccfde
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2018-01-01 18:46:46 +0800

    註冊的controller撰寫

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+    public function getRegister()
+    {
+        return view('auth/register');
+    }
+
+    protected function postRegister(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'account' => $data['account'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
+    protected function storeRegister(array $data)
+    {
+        Ticketcars::create($data->all());
+        return view('auth/login');
+    }
 }
