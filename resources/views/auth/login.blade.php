0758219700707973a68e489f9c29dd925614b61e
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2018-01-01 18:55:55 +0800

    登入頁面撰寫

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,113 @@
-@extends('layouts.app')
+<html>
+<head>
+    <title>登入訂票</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="/css/main.css" />
+    <!--[if lte IE 9]><link rel="stylesheet" href="/css/ie9.css" /><![endif]-->
+    <!--[if lte IE 8]><link rel="stylesheet" href="/css/ie8.css" /><![endif]-->
+</head>
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+<body>
+<!-- Wrapper -->
+<div id="wrapper">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+    <!-- Header -->
+    <header id="header">
+        <div class="inner">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+            <!-- Logo --><!-- Nav -->
+            <nav>
+                <ul>
+                    <li><a href="#menu">Menu</a></li>
+                </ul>
+            </nav>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+        </div>
+    </header>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+    <!-- Menu -->
+    <nav id="menu">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+        <ul>
+            <li><a href="index.php">首頁</a></li>
+            <li><a href="search.php">高鐵</a></li>
+            <li><a href="aboutus.php">關於我們</a></li>
+            <!--<li><a href="generic.html">登入</a></li>-->
+            <!--<li><a href="generic.html">訂票紀錄</a></li>-->
+            <li><a href="cutser.php">線上客服</a></li>
+        </ul>
+    </nav>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+    <!-- Main -->
+    <div id="main">
+        <div class="inner">
+            <h1>登入</h1>
+            <div class="row uniform">
+                <div class="6u 12u$(xsmall)">
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+                    <form action="/auth/login" method="POST" name="form1" id="form1">
+                        {!! csrf_field() !!}
+                        <table width="80%"  style="border:5px #000000 double; padding:5px;"  cellpadding='5' height="151" border="1">
+                            <tr>
+                                <td width="400" height="129" style="border:4px #AD0000 double;">
+                                    <p>
+                                        <label for="account">帳號：</label>
+                                        <input type="text" name="account" id="account" />
+                                    </p>
+                                    <p>
+                                        <label for="password">密碼：</label>
+                                        <input type="text" name="password" id="password" />
+                                    </p>
+                                    <p>&nbsp;</p>
+                                    <p>&nbsp;</p>
+                                    <p>
+                                        <input type="submit" name="login" id="login" value="登入" /></input>
+                                        <input name="cancle" type="submit" onClick="javascript:location.href='#'" value="取消" /></input></p>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
+                                </td>
+                            </tr>
+                        </table>
                     </form>
                 </div>
             </div>
+            <p>&nbsp;</p>
+            </ul>
+            <ul class="actions">
+
+            </ul>
+
+            </p>
+            <p>&nbsp;</p>
         </div>
     </div>
+
+    <!-- Footer -->
+    <footer id="footer">
+        <div class="inner">
+            <section>
+                <h2>Follow</h2>
+                <ul class="icons">
+                    <li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+                    <li><a href="#" class="icon style2 fa-phone"><span class="label">Phone</span></a></li>
+                    <li><a href="#" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>
+                </ul>
+            </section>
+
+        </div>
+    </footer>
 </div>
-@endsection
+<!-- Scripts -->
+<script src="js/jquery.min.js"></script>
+<script src="js/skel.min.js"></script>
+<script src="js/util.js"></script>
+<!--[if lte IE 8]><script src="js/respond.min.js"></script><![endif]-->
+<script src="js/main.js"></script>
+
+
+</body>
+</html>
+
+
