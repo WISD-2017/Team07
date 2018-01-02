f916e7fd83be7f2c30466724157b51e4ca4fa8b2
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2018-01-03 01:10:54 +0800

    修改model

--- app/User.php
+++ app/User.php
@@ -9,13 +9,9 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    protected $table = 'tickets';
     protected $fillable = [
-        'name', 'email', 'password',
+        'idnumber', 'account', 'password',
     ];
 
     /**
